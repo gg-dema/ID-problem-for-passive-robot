ae5fd48886d2d8a3b537e705871fa84f6be2cc27
Your Name Here <Same Email as used for github>
2024-01-24 18:26:16 +0100

    add pd (handmade) and integrate PID from drake, test regulation control without any particular tricks. Works for easy situation, but for any other ee-position the system is higly unstable. Added also a matlab_script for prove that the transformation choosen for solve the ID problem works

---
+++ matlab_script/ID_proveCollocatedForm_PRR.m
@@ -0,0 +1,33 @@
+clear all; 
+
+syms q1 q2 q3 l2 l3; 
+
+% mapping: h(q) = theta
+% passive output = position end-effector (x,y)
+%
+
+h_of_theta = [...
+    q1 + l2*cos(q2) + l3*cos(q2+q3);
+    l2*sin(q2) + l3*sin(q2+q3);
+    q3;
+    ];
+
+jac_h = jacobian(h_of_theta, [q1, q2, q3])
+
+%% Actuation matrix of the system: 
+
+% the robot is driven by a external force that act on the end-effector, 
+% so the actuation matrix correspond to the jacobian of the direct
+% kinematics
+
+kinematics = [  q1 + l2*cos(q2) + l3*cos(q2+q3);
+                l2*sin(q2) + l3*sin(q2+q3);
+                ];
+
+jac_ee = jacobian(kinematics, [q1, q2, q3])
+ActuationMatrix = jac_ee.'
+
+%% Solution of ID problem:
+
+% solution : DynModel = { J_h ^-T * ActuationMatrix } * F = [F1, F2, 0]
+simplify(inv(jac_h.') * ActuationMatrix)
